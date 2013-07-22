759506c9ad64c457ac0d8b63f612c5962adf76bb
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-07-22 00:34:08 -0400

    Clean up and documentation.

--- R/readLendingClubStats.r
+++ R/readLendingClubStats.r
@@ -1,4 +1,6 @@
 #' Loads Lending Club Stats into a data.frame.
+#'
+#' Note that this discards loand which do not meet the credit policy.
 #' @param path the path to the LendingClub stats.
 #' @return data.frame of Lending Club Stats.
 #' @importFrom logging logdebug
@@ -9,15 +11,17 @@
 #' stopifnot(nrow(lcstats) == 5)
 readLendingClubStats <- function(path) {
 
-  # header + first row + two empty rows + off-by-one
-  ROWS_TO_IGNORE <- 5
 
   loginfo("Reading raw Lending Club CSV.")
   lending_club_stats <- readLines(path)
 
-  # The file has two parts. We only want the first part.
+  # The file has two parts, the second of which includes only loans which
+  # do not meet the credit policy (whatever that means). We only want the first part.
   delimiter_rows <- grep('Loans that do not meet the current credit policy', lending_club_stats)
+  # header + first row + two empty rows + off-by-one
+  ROWS_TO_IGNORE <- 5
   number_of_rows <- delimiter_rows[1] - ROWS_TO_IGNORE
+
   loginfo("Loading [%s] rows", number_of_rows)
   lending_club_stats <- read.csv(path, na.strings = c('null'), skip=1, nrows=number_of_rows)
   
