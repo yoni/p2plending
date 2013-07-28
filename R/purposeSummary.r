2c73903ae9cc134da2106ce6c4ec66b2da5a984f
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-07-28 16:44:38 -0400

    Adds a summary of loan purposes.
    Also:
    * Improve documentation
    * Ignore IntelliJ droppings.

---
+++ R/purposeSummary.r
@@ -0,0 +1,26 @@
+#' Summarizes loan purposes, by default rates.
+#' @param lending_club_stats data.frame of lending club stats
+#' @return a summary of charged_off behavior by loan purpose
+#' @export
+#' @examples
+#' data(LoanStatsNew)
+#' dat <- setDateFields(LoanStatsNew)
+#' purpose_summary <- purposeSummary(dat)
+purposeSummary <- function(lending_club_stats) {
+
+  lending_club_stats <- lending_club_stats[!is.na(lending_club_stats$funded_amnt),]
+  lending_club_stats$charged_off <- lending_club_stats$loan_status == "Charged Off"
+
+  ddply(
+    lending_club_stats,
+    .(purpose, grade),
+    .fun = function(d) {
+      data.frame(
+        percent_charged_off=nrow(d[d$charged_off,])/nrow(d),
+        number_of_loans=nrow(d)
+      )
+    }
+  )
+
+}
+
