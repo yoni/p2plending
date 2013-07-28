3b140d39b055282297a03131582e3298700eb10b
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-07-28 16:54:58 -0400

    Adds a summary plot of loan purposes.

---
+++ R/plotPurposeSummary.r
@@ -0,0 +1,20 @@
+#' Plots a summary of the loan purposes.
+#' @param lending_club_stats data.frame of the lending club stats
+#' @return ggplot plot of a summary of loan performance by purpose
+#' @export
+#' @examples
+#' data(LoanStatsNew)
+#' dat <- setDateFields(LoanStatsNew)
+#' plotPurposeSummary(dat)
+plotPurposeSummary <- function(lending_club_stats) {
+  purpose_summary <- purposeSummary(lending_club_stats)
+  ggplot(
+    purpose_summary,
+    aes(
+      percent_charged_off,
+      reorder(purpose, percent_charged_off),
+      size=number_of_loans
+    )
+  ) + geom_point() + facet_grid(grade~.)
+}
+
